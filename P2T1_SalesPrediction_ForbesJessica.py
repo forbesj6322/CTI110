b8ff4bb395b27da4729485df44afb1df3b4b91cb
forbesj6322 <67401463+forbesj6322@users.noreply.github.com>
2020-06-24 22:38:14 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_ForbesJessica.py
@@ -0,0 +1,16 @@
+# Getting the projected total sales.
+# 6/20/2020
+# CTI-110 P2T1 - Sales Prediction
+# Jessica Forbes
+#
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percet of total sales
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+
